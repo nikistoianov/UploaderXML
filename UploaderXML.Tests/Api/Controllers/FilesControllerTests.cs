c564e94cf16193f01af780809f10eb0f8bf40571
Niki-ROG <nikmastera@gmail.com>
2023-11-22 15:27:06 +0200

    Test project

---
+++ UploaderXML.Tests/Api/Controllers/FilesControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UploaderXML.Api.Controllers;
+using UploaderXML.Api.DTOs;
+using UploaderXML.Api.Services;
+
+namespace UploaderXML.Tests.Api.Controllers
+{
+    public class FilesControllerTests
+    {
+        private readonly Mock<IFileService> fileServiceMock = new();
+
+        [Fact]
+        public async Task ConvertXmlToJson_WhenHasValidationError_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var response = new FileUploadResponseDto()
+            {
+                ValidationError = "Some error!"
+            };
+
+            fileServiceMock
+                .Setup(x => x.PostFileAsync(It.IsAny<IFormFile>(), It.IsAny<string>(), It.IsAny<bool>()))
+                .ReturnsAsync(response);
+            var targetController = new FilesController(fileServiceMock.Object);
+
+            // Act
+            var result = await targetController.ConvertXmlToJson(null, null, false);
+            
+            // Assert
+            result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+            result.As<ObjectResult>().Value.Should().Be(response.ValidationError);
+        }
+
+        [Fact]
+        public async Task ConvertXmlToJson_WhenHasExceptionError_ShouldReturnInternalServerError()
+        {
+            // Arrange
+            var response = new FileUploadResponseDto()
+            {
+                ExceptionMessage = "Some error!"
+            };
+
+            fileServiceMock
+                .Setup(x => x.PostFileAsync(It.IsAny<IFormFile>(), It.IsAny<string>(), It.IsAny<bool>()))
+                .ReturnsAsync(response);
+            var targetController = new FilesController(fileServiceMock.Object);
+
+            // Act
+            var result = await targetController.ConvertXmlToJson(null, null, false);
+
+            // Assert
+            result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+            result.As<ObjectResult>().Value.Should().Be(response.ExceptionMessage);
+        }
+
+        [Fact]
+        public async Task ConvertXmlToJson_WhenHasNoErrors_ShouldReturnOkObjectResult()
+        {
+            // Arrange
+            var response = new FileUploadResponseDto()
+            {
+                JsonText = "Json result"
+            };
+
+            fileServiceMock
+                .Setup(x => x.PostFileAsync(It.IsAny<IFormFile>(), It.IsAny<string>(), It.IsAny<bool>()))
+                .ReturnsAsync(response);
+            var targetController = new FilesController(fileServiceMock.Object);
+
+            // Act
+            var result = await targetController.ConvertXmlToJson(null, null, false);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            result.As<ObjectResult>().StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.As<ObjectResult>().Value.Should().Be(response.JsonText);
+        }
+    }
+}
---
+++ UploaderXML.Tests/Api/Services/FileServiceTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Xml;
+using UploaderXML.Api.BOs;
+using UploaderXML.Api.Constants;
+using UploaderXML.Api.Services;
+
+namespace UploaderXML.Tests.Api.Services
+{
+    public class FileServiceTests
+    {
+        private const string validContent = "<note><to>Me</to><body>Yess!</body></note>";
+
+        private readonly Mock<IFileWriter> fileWriterMock = new();
+
+        [Fact]
+        public async Task PostFileAsync_WhenFileDataIsMissing_ShouldReturnValidationError()
+        {
+            // Arrange
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(null, null, false);
+
+            // Assert
+            result.HasValidationError.Should().BeTrue();
+            result.ValidationError.Should().Be(ValidationErrors.NoFile);
+        }
+
+        [Fact]
+        public async Task PostFileAsync_WhenNotValidXml_ShouldReturnValidationError()
+        {
+            // Arrange
+            var content = "Fake content";
+            var fileData = MockFileData(content);
+
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(fileData, null, false);
+
+            // Assert
+            result.HasValidationError.Should().BeTrue();
+            result.ValidationError.Should().Be(ValidationErrors.InvalidXml);
+        }
+
+        [Fact]
+        public async Task PostFileAsync_WhenFileExistsAndDontOverwriteExistingFileProvided_ShouldReturnValidationError()
+        {
+            // Arrange
+            var fileData = MockFileData(validContent);
+
+            fileWriterMock
+                .Setup(x => x.FileExists(It.IsAny<string>()))
+                .Returns(true);
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(fileData, null, false);
+
+            // Assert
+            result.HasValidationError.Should().BeTrue();
+            result.ValidationError.Should().Be(ValidationErrors.ExistingFile);
+        }
+
+        [Fact]
+        public async Task PostFileAsync_WhenSomeDiskOperationFails_ShouldReturnExceptionError()
+        {
+            // Arrange
+            var fileData = MockFileData(validContent);
+            var errorMessage = "Some error!";
+
+            fileWriterMock
+                .Setup(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .ThrowsAsync(new Exception(errorMessage));
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(fileData, null, false);
+
+            // Assert
+            result.HasExceptions.Should().BeTrue();
+            result.ExceptionMessage.Should().Be(errorMessage);
+        }
+
+        [Fact]
+        public async Task PostFileAsync_WhenInvalidDirectoryProvided_ShouldReturnExceptionError()
+        {
+            // Arrange
+            var fileData = MockFileData(validContent);
+            var errorMessage = "Some error!";
+            var invalidDirectoryPath = "Invalid path";
+
+            fileWriterMock
+                .Setup(x => x.CreateDirectory(invalidDirectoryPath))
+                .Throws(new Exception(errorMessage));
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(fileData, invalidDirectoryPath, false);
+
+            // Assert
+            result.HasExceptions.Should().BeTrue();
+            result.ExceptionMessage.Should().Be(errorMessage);
+        }
+
+        [Fact]
+        public async Task PostFileAsync_WhenValidDataProvided_ShouldReturnJsonResultWithoutErrors()
+        {
+            // Arrange
+            var fileData = MockFileData(validContent);
+            var document = new XmlDocument();
+            document.LoadXml(validContent);
+            var expectedResult = JsonConvert.SerializeXmlNode(document, Newtonsoft.Json.Formatting.Indented);
+            var targetService = new FileService(fileWriterMock.Object);
+
+            // Act
+            var result = await targetService.PostFileAsync(fileData, null, true);
+
+            // Assert
+            result.HasExceptions.Should().BeFalse();
+            result.HasValidationError.Should().BeFalse();
+            result.JsonText.Should().Be(expectedResult);
+        }
+
+        private IFormFile MockFileData(string content)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            IFormFile file = new FormFile(stream, 0, stream.Length, "id_from_form", "test.ext");
+
+            return file;
+        }
+    }
+}
